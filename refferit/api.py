867e3f58d822eaea19ccfa354d93ba6241af63f3
umaepoch <umag@epochconsulting.in>
2020-08-11 22:46:56 +0530

    api

--- refferit/api.py
+++ refferit/api.py
@@ -7,18 +7,19 @@ import datetime
 def auto_points():
 	reffer_point = frappe.db.sql(""" select * from `tabreferral`""", as_dict = 1)
 	data = []
-	for ref in reffer_point:
-		key = ref.referre_to
-		if len(data) != 0:
-			for d in data:
-				if key in [d['referre_to'] for d in data]:
-					d.update({"referre_to":key ,"point": d['point']+1})
-				else:
-					data.append({"referre_to":key , "point": 1})
-		else:
-			data.append({"referre_to":key , "point": 1})
-	f= open("/home/frappe/frappe-bench/apps/refferit/refferit/refferit/output.out","a+")
-	f.write("data---------------------------"+str(data)+"\n")
-	for d in data:
-		f.write("d.referre_to---------------------------"+str(d['referre_to'])+"\n")
-		frappe.db.set_value("Contact", d['referre_to'], "points", d['point'])
+	if len(reffer_point) != 0:
+		for ref in reffer_point:
+			key = ref.referre_to
+			if len(data) != 0:
+				for d in data:
+					if key in [d['referre_to'] for d in data]:
+						d.update({"referre_to":key ,"point": d['point']+1})
+					else:
+						data.append({"referre_to":key , "point": 1})
+			else:
+				data.append({"referre_to":key , "point": 1})
+		f= open("/home/frappe/frappe-bench/apps/refferit/refferit/refferit/output.out","a+")
+		f.write("data---------------------------"+str(data)+"\n")
+		for d in data:
+			f.write("d.referre_to---------------------------"+str(d['referre_to'])+"\n")
+			frappe.db.set_value("Contact", d['referre_to'], "points", d['point'])
